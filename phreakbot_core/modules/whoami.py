0529575eb04405729ddde5436065ef8bb1c66eb4
Johan Sebastiaan Koetsier <skoetsier@meta.com>
2025-07-06 20:21:41 +0800

    Fix whoami.py module to use the new PhreakBot API

--- phreakbot_core/modules/whoami.py
+++ phreakbot_core/modules/whoami.py
@@ -1,40 +1,44 @@
-def config(wcb):
+def config(bot):
     return {
-        'events': [],
-        'commands': ['whoami', 'test'],
-        'permissions': [],
-        'help': "Tells you who the bot thinks you are."
+        "events": [],
+        "commands": ["whoami", "test"],
+        "permissions": ["user"],
+        "help": "Tells you who the bot thinks you are.",
     }
 
 
-def run(wcb, event):
-    if 'test =' in event['text']: # ignore '!test = bla' events.
+def run(bot, event):
+    if "test =" in event["text"]:  # ignore '!test = bla' events.
         return
 
-    rtxt = "you are %s at %s" % (event['nick'], event['nickmask'])
+    rtxt = f"You are {event['nick']} at {event['hostmask']}"
 
-    if event['nickmask'] == wcb.state['bot_ownermask']:
+    if event["hostmask"] == bot.config["owner"]:
         rtxt += ", and YOU are my owner!"
     else:
-        if event['user_info'] and event['user_info']['username']:
-            rtxt += ", registered user " + event['user_info']['username']
+        if event["user_info"] and event["user_info"].get("username"):
+            rtxt += f", registered user {event['user_info']['username']}"
 
-            if event['user_info']['permissions']['global']:
-                rtxt += ", with global perms (%s)" % ", ".join(event['user_info']['permissions']['global'])
+            if event["user_info"]["permissions"]["global"]:
+                rtxt += f", with global perms ({', '.join(event['user_info']['permissions']['global'])})"
 
-            channel = event['channel']
-            if channel in event['user_info']['permissions']:
-                rtxt += ", with " + channel + " perms (" + ", ".join(event['user_info']['permissions'][channel]) + ")"
+            channel = event["channel"]
+            if channel in event["user_info"]["permissions"]:
+                rtxt += f", with {channel} perms ({', '.join(event['user_info']['permissions'][channel])})"
 
         else:
-            rtxt += ", unrecognised user"
+            rtxt += ", unrecognized user"
 
-            db = wcb.db_connect()
-            cur = db.cursor()
-            sql = "SELECT * FROM wcb_users WHERE username ILIKE %s"
-            cur.execute(sql, (event['nick'].lower(),))
-            res = cur.fetchone()
-            if res:
-                rtxt += ", but a user was found in the DB, perhaps you need a merge?"
+            if bot.db_connection:
+                try:
+                    cur = bot.db_connection.cursor()
+                    sql = "SELECT * FROM phreakbot_users WHERE username ILIKE %s"
+                    cur.execute(sql, (event["nick"].lower(),))
+                    res = cur.fetchone()
+                    if res:
+                        rtxt += ", but a user was found in the DB, perhaps you need a merge?"
+                    cur.close()
+                except Exception as e:
+                    bot.logger.error(f"Database error in whoami module: {e}")
 
-    return wcb.reply(rtxt)
+    bot.add_response(rtxt)
