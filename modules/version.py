d719966776adb2a2c3622d4e592bbcd3a6ff091f
Johan Sebastiaan Koetsier <skoetsier@meta.com>
2025-07-06 22:48:00 +0800

    Add rewritten version module

---
+++ modules/version.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Version module for PhreakBot
+
+
+def config(bot):
+    """Return module configuration"""
+    return {
+        "events": [],
+        "commands": ["v", "version"],
+        "permissions": ["user"],
+        "help": "Shows information about the bot.\n"
+               "Usage: !version - Display bot version and repository information",
+    }
+
+
+def run(bot, event):
+    """Handle version commands"""
+    bot.add_response("PhreakBot - https://github.com/jskoetsier/phreakbot/")
