6fc62e7dee6f2d7edba4b42c588932019366a58c
Johan Sebastiaan Koetsier <skoetsier@meta.com>
2025-07-07 02:24:02 +0800

    Fixed remaining wcb references in topic.py

--- modules/topic.py
+++ modules/topic.py
@@ -8,7 +8,7 @@
 #
 
 
-def config(wcb):
+def config(pb):
     return {
         "events": [
             "irc_in2_332",
@@ -24,18 +24,18 @@ def config(wcb):
     }
 
 
-def run(wcb, event):
+def run(pb, event):
     # Handle topic response events
     if event["trigger"] == "event":
         if event["signal"] == "irc_in2_332":  # RPL_TOPIC response
             # This is the response to a TOPIC request
             channel = event["raw_event"].arguments[0]
             topic = event["raw_event"].arguments[1]
-            wcb.say(channel, f"Current topic: {topic}")
+            pb.say(channel, f"Current topic: {topic}")
         elif event["signal"] == "irc_in2_TOPIC":  # TOPIC change notification
             channel = event["raw_event"].target
             topic = event["raw_event"].arguments[0]
-            wcb.say(channel, f"Topic changed to: {topic}")
+            pb.say(channel, f"Topic changed to: {topic}")
         return
 
     # Handle commands
@@ -46,70 +46,70 @@ def run(wcb, event):
             channel = event["command_args"].strip()
 
         # Check if the channel is in the config
-        if channel not in wcb.config["channels"]:
-            wcb.reply(f"I'm not in channel {channel}")
+        if channel not in pb.config["channels"]:
+            pb.reply(f"I'm not in channel {channel}")
             return
 
         # Get the topic
-        wcb.logger.info(f"Getting topic for channel: {channel}")
-        wcb.connection.topic(channel)
-        wcb.reply(f"Retrieving topic for {channel}...")
+        pb.logger.info(f"Getting topic for channel: {channel}")
+        pb.connection.topic(channel)
+        pb.reply(f"Retrieving topic for {channel}...")
 
     elif event["command"] == "settopic":
         # Set the topic
         if not event["command_args"]:
-            wcb.reply("Please provide a topic")
+            pb.reply("Please provide a topic")
             return
 
         channel = event["channel"]
         topic = event["command_args"]
 
         # Check if the user has permission to set topics
-        has_permission = wcb._check_permissions(event, ["topic"])
-        wcb.logger.info(
+        has_permission = pb._check_permissions(event, ["topic"])
+        pb.logger.info(
             f"User {event['nick']} permission to set topic: {has_permission}"
         )
 
         if not has_permission:
-            wcb.reply("You don't have permission to set topics.")
+            pb.reply("You don't have permission to set topics.")
             return
 
         # Check if the channel is in the config
-        if channel not in wcb.config["channels"]:
-            wcb.reply(f"I'm not in channel {channel}")
+        if channel not in pb.config["channels"]:
+            pb.reply(f"I'm not in channel {channel}")
             return
 
         # Set the topic
-        wcb.logger.info(f"Setting topic in {channel} to: {topic}")
-        wcb.connection.topic(channel, topic)
-        wcb.reply(f"Setting topic for {channel} to: {topic}")
+        pb.logger.info(f"Setting topic in {channel} to: {topic}")
+        pb.connection.topic(channel, topic)
+        pb.reply(f"Setting topic for {channel} to: {topic}")
 
     elif event["command"] == "addtopic":
         # Add to the topic
         if not event["command_args"]:
-            wcb.reply("Please specify text to add to the topic.")
+            pb.reply("Please specify text to add to the topic.")
             return
 
         channel = event["channel"]
         addition = event["command_args"]
 
         # Check if the user has permission to set topics
-        has_permission = wcb._check_permissions(event, ["topic"])
-        wcb.logger.info(
+        has_permission = pb._check_permissions(event, ["topic"])
+        pb.logger.info(
             f"User {event['nick']} permission to modify topic: {has_permission}"
         )
 
         if not has_permission:
-            wcb.reply("You don't have permission to modify topics.")
+            pb.reply("You don't have permission to modify topics.")
             return
 
         # Check if the channel is in the config
-        if channel not in wcb.config["channels"]:
-            wcb.reply(f"I'm not in channel {channel}")
+        if channel not in pb.config["channels"]:
+            pb.reply(f"I'm not in channel {channel}")
             return
 
         # For now, just set the new topic to the addition
         # In a future update, we could implement a callback to get the current topic first
-        wcb.connection.topic(channel, addition)
-        wcb.reply(f"Setting topic to: {addition}")
+        pb.connection.topic(channel, addition)
+        pb.reply(f"Setting topic to: {addition}")
     # This comment is already handled in the code above
